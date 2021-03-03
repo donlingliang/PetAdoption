fd7236bbf787c4c65845fa2ec089040249af4927
Don Liang <donliangldl@gmail.com>
2021-03-02 21:27:01 -0800

    removed the wildcard import

--- app/src/main/java/com/example/androiddevchallenge/PetInfoActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/PetInfoActivity.kt
@@ -24,7 +24,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.ripple.rememberRipple
